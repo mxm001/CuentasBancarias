7a1c3bd8e71549b6dfdd8c548f46ef503d8e957f
Maxi Mour <moure.lm@gmail.com>
2017-05-08 01:17:51 -0300

    Test Basicos TODO:Agregar complejidad

--- CuentasBancarias/test/ar/edu/unlam/pb2/TestCuentas.java
+++ CuentasBancarias/test/ar/edu/unlam/pb2/TestCuentas.java
@@ -1,5 +1,51 @@
 package ar.edu.unlam.pb2;
 
-public class TestCuentas {
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+
+public class TestCuentas {
+	@Test
+	public void TestCuentaExtraccion() {
+		Cuenta MiCuenta =new Cuenta();
+		MiCuenta.setSaldo(40.00);
+		MiCuenta.hacerExtraccion(20.00);
+		Assert.assertEquals(20.00, MiCuenta.getSaldo());
+	}
+	@Test
+	public void TestCuentaCorrienteExtraccion() {
+		CuentaCorriente MiCuenta =new CuentaCorriente();
+		MiCuenta.setSaldo(40.00);
+		MiCuenta.hacerExtraccion(20.00);
+		Assert.assertEquals(20.00, MiCuenta.getSaldo());
+	}
+	@Test
+	public void TestCajaAhorroExtraccion() {
+		CajaAhorro MiCuenta =new CajaAhorro();
+		MiCuenta.setSaldo(40.00);
+		MiCuenta.hacerExtraccion(20.00);
+		Assert.assertEquals(20.00, MiCuenta.getSaldo());
+	}
+	@Test
+	public void TestCuentaDeposito() {
+		Cuenta MiCuenta =new Cuenta();
+		MiCuenta.hacerDeposito(20.00);
+		Assert.assertEquals(20.00, MiCuenta.getSaldo());
+	}
+	@Test
+	public void TestCuentaCorrienteDeposito() {
+		CuentaCorriente MiCuenta =new CuentaCorriente();
+		MiCuenta.hacerDeposito(20.00);
+		Assert.assertEquals(20.00, MiCuenta.getSaldo());
+	}
+	@Test
+	public void TestCajaAhorroDeposito() {
+		CajaAhorro MiCuenta =new CajaAhorro();
+		MiCuenta.hacerDeposito(20.00);
+		Assert.assertEquals(20.00, MiCuenta.getSaldo());
+	}
 }
